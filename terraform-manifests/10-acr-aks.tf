e6710d4283b482e48b85eb236bfb093919e2a4cf
CapstoneProjectItc <140419054+ItcTrainee2023@users.noreply.github.com>
2023-09-08 01:55:40 +0100

    Added Trigger Main

--- terraform-manifests/10-acr-aks.tf
+++ terraform-manifests/10-acr-aks.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acrrg01" {
-  name                = "AKS-Forge_ACR"
+  name                = "AKSForgeACR"
   resource_group_name = azurerm_resource_group.aks_rg.name
   location            = azurerm_resource_group.aks_rg.location
   sku                 = "Standard"
